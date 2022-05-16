bc59a483a71e04fa7e691392fd3bcbe374ac091a
Zach <shaw.zacht@gmail.com>
2022-05-15 22:25:52 -0700

    commit tree 8383b593595d6d7c877265b28dbc6b4c20292a74

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") 
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentences = []
+    self.split(/[.?!]/).map do |element|
+      element.length > 0 ? sentences.push(element) : nil
+    end
+    sentences.length
   end
 end
